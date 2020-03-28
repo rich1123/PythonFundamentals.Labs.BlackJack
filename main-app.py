07200c40a630d0d106a1fe08a831e539ad830d2d
Rich Maiale <richm1123@gmail.com>
2020-03-28 17:42:45 -0400

    added first complete function hand_val ABOUT TO UNITTEST (HOLD YOUR BREATH) 3/28

--- main-app.py
+++ main-app.py
@@ -1,12 +1,26 @@
 import card_detail
+import bjack_test
 
-comp_hand {}
+comp_hand = {}
 
-player_hand {}
+player_hand = {}
+
+player_move = {
+    1: "Hit",
+    2: "Stand",
+    3: "Leave the Game",
+}
 
 
 def intro():
+    print("Welcome to my BlackJack table\n")
+    print(f"Dealer's Hand showing {hand_val(comp_hand)}")
+    input("")
 
+def hand_val(d: dict):
+    for v in d:
+        sum(d.values())
+        return sum
 
 def comp_build():
 
