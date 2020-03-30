534652fa8dbf625e5fa2ad1ed1d08b59bf6dbb19
Rich Maiale <richm1123@gmail.com>
2020-03-29 20:51:40 -0400

    updated main_app file name (possible import issues)... unit test on 'move_card' failing on 2nd arg for assertEqual

---
+++ card_detail.py
@@ -0,0 +1,61 @@
+# def card_val():
+
+
+# class Deck:
+
+# def __init__(self):
+
+suit = ['Spades', 'Hearts', 'Clubs', 'Diamonds']
+
+worth = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
+
+deck = {}
+
+def hand_val(hand: dict):
+    # for v in hand:
+    v_sum = sum(hand.values())
+    return v_sum
+
+def blackjack(v_sum: int):
+    if v_sum > 21:
+        print(f"You busted: {v_sum}")
+    if v_sum == 21:
+        print(f"Big Winner!!! {v_sum}")
+    else:
+        pass
+
+#
+# def card_detail():
+#     for s in suit:
+#         for w in worth:
+#             card = {s, worth[w]}
+#             deck.update(card)
+#             # break
+#         # s += 1
+#         # w += len(worth)
+#         print(deck)
+
+
+print(deck)
+
+
+    #     # s <= len(suit[])
+    #     x = 0
+    #     deck[suit[s]] = worth[x]
+    #     deck.update(s)
+    #     s += 1
+    #     s <= len(suit)
+    #     # w += 1
+    #     return deck
+
+# print()
+
+
+def main():
+    card_detail()
+
+#
+#
+#
+if __name__ == '__main__':
+    main()
