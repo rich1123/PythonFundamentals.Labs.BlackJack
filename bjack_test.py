41c805fc34ecaea36ea1d838f0f0bf59f194f984
Rich Maiale <richm1123@gmail.com>
2020-04-01 00:25:23 -0400

    new unit tests

--- bjack_test.py
+++ bjack_test.py
@@ -1,31 +1,59 @@
 import unittest
 from mock import Mock
 from unittest.mock import patch
-import main_app
+# import main_app
 import random
+from main_app import move_card, mv_random_card, comp_build
 
 class TestBjack(unittest.TestCase):
 
+    def setUp(self) -> None:
+        self.deck_dict = {}
+        self.ht_dict = {'4 of C': 4}
+        self.ht_dict2 = {'A of C': 1}
+        self.ht_dict3 = {'2 of C': 2}
+        self.ht_dict4 = {'3 of C': 3}
+
     def test_move_card(self):
+
+        # deck_dict = {}
+        #
+        # ht_dict = {'4 of C': 4}
+
+        move_card(self.ht_dict, self.deck_dict, '4 of C')
+        self.assertDictEqual(self.deck_dict, {'4 of C': 4})
+        self.assertDictEqual({}, self.ht_dict)
+
+    def test_mv_random_card(self):
         """unit test for
-    def move_card(from_hand: dict, to_hand: dict, card:str):
-    to_hand[card] = from_hand.pop(card)"""
-        # def __name__
-        test_dict = \
-        {'9 of C': 9,
-                 '6 of C': 6,
-                 '8 of C': 8,
-                 '7 of C': 7,
-                 }
+    def mv_random_card(to_hand: dict):
+    random_card = random.choice(list(undealt.keys()))
+    move_card(undealt, to_hand, random_card)"""
 
-        ht_dict = {}
+        # test_dict = {'9 of C': 9,
+        #              '6 of C': 6,
+        #              '8 of C': 8,
+        #              '7 of C': 7,
+        #              }
+        # ht_dict = {'6 of H': 6, '7 of H': 7, '8 of H': 8, '9 of H': 9, }#
 
-        test = {'key': 'value'}
-        original = ht_dict[test].copy()
-        with patch.dict(test, {'9 of C': 9}, clear=True):
-            assert test == original
+        mv_random_card(self.ht_dict)
+        result = '4 of C'
+        self.assertEqual(result, random.choice(list(self.ht_dict.keys())))
+        self.assertEqual('A of C', random.choice(list(self.ht_dict2.keys())))
+        self.assertEqual('2 of C', random.choice(list(self.ht_dict3.keys())))
+        self.assertEqual('3 of C', random.choice(list(self.ht_dict4.keys())))
 
-        # result = main_app.move_card(test_dict, ht_dict, card="'9 of C': 9")
+
+    def test_comp_build(self):
+        comp_build(('4 of C': 4))
+        self.assertDictEqual()
+        # test = {'key': 'value'}
+        # original = ht_dict[test].copy()
+        # with patch.dict(test, {'9 of C': 9}, clear=True):
+        #     assert test == original
+        #
+        # # result = main_app.move_card(test_dict, ht_dict, card="'9 of C': 9")
         # self.assertEqual(result, ht_dict={)
 
         # mock = Mock(result=ht_dict[{'9 of C': 9}])
@@ -34,22 +62,6 @@ class TestBjack(unittest.TestCase):
 
         # (from_hand: dict, to_hand: dict, card: str):
         #     to_hand[card] = from_hand.pop(card)
-
-    def test_mv_random_card(self):
-        """unit test for
-    def mv_random_card(to_hand: dict):
-    random_card = random.choice(list(undealt.keys()))
-    move_card(undealt, to_hand, random_card)"""
-
-        test_dict = {'9 of C': 9,
-                     '6 of C': 6,
-                     '8 of C': 8,
-                     '7 of C': 7,
-                     }
-        ht_dict = {'6 of H': 6, '7 of H': 7, '8 of H': 8, '9 of H': 9,}
-
-        result = main_app.mv_random_card(ht_dict)
-        self.assertEqual(result, random.choice(list(test_dict.keys())))
 
 
 
